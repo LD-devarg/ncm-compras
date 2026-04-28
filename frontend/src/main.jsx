343e771cc8f80ab844f7457420fd4adcb586e41a
LD.dev <ld.devarg@gmail.com>
2026-04-28 00:37:54 -0300

    fix: usar HashRouter para compatibilidad con GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import OC from './OC.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ncm-compras">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/oc" element={<OC />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
